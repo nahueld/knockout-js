7152092bb34eadeb0471df7cda73ef19c8daaf9f
nahueld <nahuel.dealbera@gmail.com>
2018-08-09 16:37:34 +1000

    Removing class from App

--- src/App.vm.js
+++ src/App.vm.js
@@ -3,29 +3,27 @@ import Actions from "./state/Actions";
 
 require("knockout.validation");
 
-class App {
-  constructor() {
-    let state = new State();
-    let actions = new Actions(state);
+function App() {
+  let state = new State();
+  let actions = new Actions(state);
 
-    this.appState = {
-      leaderForm: {
-        fields: state.formFields,
-        onSubmit: actions.submitLeaderForm
+  this.appState = {
+    leaderForm: {
+      fields: state.formFields,
+      onSubmit: actions.submitLeaderForm
+    },
+    leadersList: {
+      entries: state.leaderEntries,
+      sortingField: {
+        ...state.sortingField,
+        onChange: actions.sortLeadersList
       },
-      leadersList: {
-        entries: state.leaderEntries,
-        sortingField: {
-          ...state.sortingField,
-          onChange: actions.sortLeadersList
-        },
-        orderingField: {
-          ...state.orderingField,
-          onChange: actions.sortLeadersList
-        }
+      orderingField: {
+        ...state.orderingField,
+        onChange: actions.sortLeadersList
       }
-    };
-  }
+    }
+  };
 }
 
 export default App;
